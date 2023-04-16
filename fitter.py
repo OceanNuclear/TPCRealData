19b09f7c44af9a497f4ccd24712b4a1a62d21a1d
ocean <OceanWongUK@gmail.com>
2023-04-16 02:13:27 +0100

    added the outline feature properly. Took more work than expected. Next step is to extract the backbone by fitting simple dome-shaped distributions onto the tracks.

--- fitter.py
+++ fitter.py
@@ -76,24 +76,71 @@ zero_sum_oct = ary([
                     [ 2, 0, 2],
                     [ 1, 2, 1],])/12
 
+_clockwise_matrix = ary([[0,1],[-1,0]])
+_anti_clockwise_matrix = ary([[0,-1],[1,0]])
+
+
+
 class BoundaryCrawler():
     """A crawler that will record the path of the boundary"""
-    def __init__(self, blob_map):
-        self.blob_map = blob_map
+    def __init__(self, blob_map_bool):
+        """blob_map_bool: 2D boolean numpy array."""
+        self.blob_map = blob_map_bool
         self.boundary_list = []
-        self.interior_node = ary([-1, -1])
-        self.exterior_node = ary([-1, -1])
+        self.interior_leg = ary([None, None])
+        self.exterior_leg = ary([None, None])
 
-    def initialize_west(self):
-        """Initilize a crawler to be at at the east-most position of the map"""
+    def is_occupied(self, coordinate):
+        if coordinate[0] not in range(self.blob_map.shape[0]):
+            return False
+        elif coordinate[1] not in range(self.blob_map.shape[1]):
+            return False
+        else:
+            return self.blob_map[tuple(coordinate)]
+
+    def is_empty(self, coordinate):
+        return ~self.is_occupied(coordinate)
+
+    def initialize_north(self):
+        """Initilize a crawler to be at at the north-most position of the map"""
         interior_cells = ary(np.where(self.blob_map)).T
-        index_of_start_point_chosen = np.argmin(interior_cells[:, 1])[0]
-        interior_cells[index_of_start_point_chosen]
-        return
+        index_of_start_point_chosen = np.argmin(interior_cells[:, 0])
+        self.interior_leg = interior_cells[index_of_start_point_chosen]
+        self.exterior_leg = self.interior_leg + ary([-1, 0]) # shift it to one higher in the y coordinate.
+        self.boundary_list.append(np.mean([self.exterior_leg, self.interior_leg], axis=0))
+        # this should be 
+        return self.boundary_list[-1]
 
     def step_clockwise(self):
         """Go clockwise around the block"""
-        return
+        int_to_ext = self.exterior_leg - self.interior_leg
+        up = _clockwise_matrix@int_to_ext
+        config1 = ary([0, 0]), up + (_anti_clockwise_matrix@up)
+        config2 = up, up
+        config3 = up + (_clockwise_matrix@up), ary([0, 0])
+
+        if   self.is_empty(self.exterior_leg+config1[0]) and self.is_occupied(self.interior_leg+config1[1]):
+            self.exterior_leg += config1[0]
+            self.interior_leg += config1[1]
+        elif self.is_empty(self.exterior_leg+config2[0]) and self.is_occupied(self.interior_leg+config2[1]):
+            self.exterior_leg += config2[0]
+            self.interior_leg += config2[1]
+        elif self.is_empty(self.exterior_leg+config3[0]) and self.is_occupied(self.interior_leg+config3[1]):
+            self.exterior_leg += config3[0]
+            self.interior_leg += config3[1]
+        else:
+            raise RuntimeError("Crawler stuck!")
+        self.boundary_list.append(np.mean([self.exterior_leg, self.interior_leg], axis=0))
+        return self.boundary_list[-1]
+
+    def run(self):
+        """Perform one complete cycle around the blob, marking out its boundary."""
+        self.initialize_north()
+        start_point = self.boundary_list[0].copy()
+        self.step_clockwise()
+        while not np.array_equal(self.boundary_list[-1], start_point):
+            self.step_clockwise()
+        return self.boundary_list
 
 def read_raw_pixel_values(filename):
     """Get the raw RBG values"""
@@ -104,70 +151,6 @@ class OperationOrderError(Exception):
     pass
 
 _mnorm = lambda x: x/x.max()
-
-def find_neighbours_index_in_list(remaining_coordinates, cell_of_interest):
-    """Given a list of coordinates, and the current cell being studied (cell_of_interest),
-    return indices i, j, k ... etc. such that list[i], list[j], list[k]... etc.
-    are the coordinates of the cell neighbouring the current cell being studied."""
-    L_infty_distance = abs(ary(remaining_coordinates) - cell_of_interest).max(axis=1)
-    return np.where(L_infty_distance==1)[0]
-
-def choose_neightbouring_coordinatesL1(coordinate_list, cell_of_interest):
-    """Similar to find_neighbours_index_in_list,
-    but instead of returning the index (i.e. position) of the closest point in the list,
-        it returns the actual coordinate itself.
-    This method also uses define neighbour as L1 distance =1 ,i.e. there are 4 neighbours instead of 8."""
-    Linfty_distance = abs(ary(coordinate_list) - cell_of_interest).sum(axis=1)
-    indices = np.where(Linfty_distance==1)[0]
-    return ary(coordinate_list)[indices]
-
-def sort_into_line(list_of_coordinates, selected_area):
-    """
-    Parameter
-    ---------
-    A list of integer coordinates, forming an unbroken line.
-    """
-    # catch the unusual cases (0 points or 1 point) up front.
-    if len(list_of_coordinates)<2:
-        return list(list_of_coordinates)
-
-    # scroll through the list of boundary points until you find one with exactly 2 neighbours.
-    for i in range(len(list_of_coordinates)):
-        copied_list = list(list_of_coordinates)
-        # Record 1st point.
-        output_list = [copied_list.pop(i),]
-        neighbour_indices = find_neighbours_index_in_list(copied_list, output_list[-1])
-        if len(neighbour_indices)<=2: # between 1-2 neighbours
-            chosen_point_index = neighbour_indices[0]
-            break
-    else:
-        raise ValueError("Failed to find a starting point for the outline! Not a contiguous boundary line provided?")
-
-    while len(copied_list)>1:
-        # Record next point
-        output_list.append(copied_list.pop(chosen_point_index))
-
-        neighbour_indices = find_neighbours_index_in_list(copied_list, output_list[-1])
-
-        if len(neighbour_indices)==1:
-            # choose the only point available.
-            chosen_point_index = neighbour_indices[0]
-            continue
-        elif len(neighbour_indices)>1:
-            # comparison is required to figure out which one is the closest one.
-            empty_space_coords = ary(np.where(~selected_area)[::-1]).T
-            empty_neighbours_of_current_point = choose_neightbouring_coordinatesL1(empty_space_coords, output_list[-1])
-            for i, nb in zip(neighbour_indices, ary(copied_list)[neighbour_indices]):
-                # one and only ONE of these points shall share the thing.
-                if len(find_neighbours_index_in_list(empty_neighbours_of_current_point, nb))>0:
-                    chosen_point_index = i
-                    break
-            else:
-                raise ValueError("None of the neighbouring boundary point shares a mutual empty cell neighbour with the current point!")
-        else: # len(neighbour_indices)<1:
-            raise ValueError("No matching neighbours were found")
-
-    return output_list
 
 class Event():
     def __init__(self, filename):
@@ -253,7 +236,7 @@ class Event():
                 #     new_signal_blobs[selected_cells] = 0 
             setattr(self, strip_direction+"_blobs", new_signal_blobs)
 
-    def clean_blobs(self, prominence_threshold=1.5, fill_holes=True):
+    def clean_blobs(self, prominence_threshold=1.5, fill_holes=False):
         """Remove any blobs whose max. prominence does not reach the theshold"""
         for strip_direction in "uvw":
             if not hasattr(self, strip_direction+"_blobs"):
@@ -299,28 +282,15 @@ class Event():
                 raise OperationOrderError("self.{u,v,w}_blobs must've been first populated by self.label_large_chunks and expanded upon using self.expand_blob")
             blob_map = getattr(self, strip_direction+"_blobs")
 
-            outline_lists = []
+            outline_lists = list()
             for blob_label in np.unique(blob_map.flatten()):
                 if blob_label==0:
                     continue
                 selected_cells = blob_map==blob_label
-                wetted_by_empty_cells = convolve2d(~selected_cells, zero_sum_quad, 'same')>0
-                highlighted_boundary = np.logical_and(wetted_by_empty_cells, selected_cells)
-                coordinates_of_boundary = ary(np.where(highlighted_boundary)[::-1]).T
-                sorted_list_of_boundary = sort_into_line(coordinates_of_boundary, selected_cells)
-                outline_lists.append(sorted_list_of_boundary)
-
-                # fig, (ax0, ax1) = plt.subplots(2)
-                fig, ax0 = plt.subplots()
-                ax0.imshow(getattr(self, strip_direction))
-                for line in outline_lists:
-                    ax0.plot(*(ary(line).T), color='red')
-                # ax1.imshow(is_boundary, alpha=0.5)
-                plt.show()
-            # 3-neighbours: concave, should be filled in anyways.
-            # 2-neighbours: yes, is edge.
-            # 1-neighbour: vertex.
-            # draw convex hull with the boundary here?
+                crawler = BoundaryCrawler(selected_cells)
+                boundary = crawler.run()
+                outline_lists.append(boundary)
+            setattr(self, strip_direction+"_outline_lists", outline_lists)
 
     def triangulate(self):
         pass
@@ -346,24 +316,27 @@ if __name__=="__main__":
         if num_prongs!=3:
             continue
         for i in range(0,371):
-            if i not in (1, 16, 17, 20):
+            # if i not in (1, 16, 17, 20,):
                 # Use 3-prong: evt1, 16, 17 .png as the examples.
+            if i >20:
                 continue
             event_name = "train/merged_{}p/evt{}.png".format(num_prongs, i)
             event = Event(event_name)
-            event.label_large_chunks(97, 40)
+            event.label_large_chunks(95, 40)
             event.expand_blob()
             event.clean_blobs()
             event.calculate_outline()
 
-            fig, (ax0, ax1) = plt.subplots(2)
+            fig, ax0 = plt.subplots(1)
             fig.suptitle(f"{num_prongs}-prongs evt{i}.png")
             ax0.imshow(ary([event.u, event.v, event.w]).transpose([1,2,0])
                 /(len(TRANSLATION_TABLE)-1))
-            ax1.imshow(ary([ary((event.u_blobs)>0, dtype=float),
-                            ary((event.v_blobs)>0, dtype=float),
-                            ary((event.w_blobs)>0, dtype=float)]).transpose([1,2,0]))
-            ax1.set_title("Extracted Outlines of the Event")
+            
+            [ax0.plot(*(ary(line).T[::-1]), color='red') for line in event.u_outline_lists]
+            [ax0.plot(*(ary(line).T[::-1]), color='green') for line in event.v_outline_lists]
+            [ax0.plot(*(ary(line).T[::-1]), color='blue') for line in event.w_outline_lists]
+
+            ax0.set_title("Overlayed by extracted outlines")
             plt.tight_layout()
             try:
                 plt.show()
